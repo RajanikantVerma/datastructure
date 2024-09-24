e3d06fedeb1ebe45bed4b8670166c65d83ae4508
RajanikantVerma <rajanikantnitmz@gmail.com>
2024-09-24 23:42:32 +0530

    Create Node.java

---
+++ Trie/Node.java
@@ -0,0 +1,24 @@
+public class Node {
+	boolean flag = false;
+    Node[] list = new Node[10];
+
+    void put(Node new_node, char ch) {
+        list[ch - '0'] = new_node;
+    }
+
+    boolean containKey(char ch) {
+        return list[ch - '0'] != null;
+    }
+
+    Node get(char ch) {
+        return list[ch - '0'];
+    }
+
+    void setEnd() {
+        flag = true;
+    }
+
+    boolean isEnd() {
+        return flag;
+    }
+}
